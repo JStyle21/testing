05c22390ddf3e089c6f18af2b186aaf0a371c178
JStyle21 <unfold1911@gmail.com>
2021-01-31 17:04:14 +0200

    ready to test

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+    zz = { 1: 1, 2: 2, 3: 3 };
+    res.render('reg', zz);
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on Port: ${port}`);
+});
